c2a6ab7b287937e269395c33a44f2145dab95db3
YYL1999 <2291003927@qq.com>
2019-07-17 14:23:21 +0800

    feat(login): add mysql data

--- models/login.go
+++ models/login.go
@@ -1,6 +1,9 @@
 package models
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/astaxie/beego/orm"
 )
 
@@ -31,6 +34,15 @@ func GetUserById(ID int64) (*User, error) {
 	return user, err
 }
 
+//查询用户名
+func GetUserByName(name string) (*User, error) {
+	o := orm.NewOrm()
+	user := new(User)
+	qs := o.QueryTable("user")
+	err := qs.One(user)
+	return user, err
+}
+
 //手机号查询账号
 func GetUserByTel(tel string) (*User, error) {
 	o := orm.NewOrm()
@@ -38,4 +50,21 @@ func GetUserByTel(tel string) (*User, error) {
 	qs := o.QueryTable("user")             //表名为t_user
 	err := qs.Filter("tel", tel).One(user) //One是指查询一条数据，One(user)是查询mysql表中一条数据
 	return user, err
+}
+
+//插入新用户
+func InsertUser(tel string, name string, password string, id string) (*User, error) {
+	b, error := strconv.Atoi(tel)
+	c, error := strconv.Atoi(id)
+	if error != nil {
+		fmt.Println("字符串转换成整数失败")
+	}
+	o := orm.NewOrm()
+	var user User
+	user.Name = name
+	user.Password = password
+	user.ID = c
+	user.Tel = b
+	_, err := o.Insert(&user)
+	return nil, err
 }
